2bfb44eb6f7636e7a9f58d50aa8aa71c5ac05ee5
Angela A <ranran.wang@wework.com>
2018-03-12 16:18:50 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,14 +16,43 @@ WIN_COMBINATIONS = [
   [6,4,2],
 ]
 
-def #won()
-  if board = []
-    return "false"
-  elsif board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
-    return "false"
-  elsif board = WIN_COMBINATIONS
-    return "true"
-   end 
+def won(board)
+  if !board.inclue? "X" and !board.include? "O"
+    return false
+  end
+  winner = false 
+  WIN_COMBINATIONS.each do |combo|
+    winningIndex1 = combo[0]
+    winningIndex2 = combo[1]
+    winningIndex3 = combo[2]
+    
+    check1 = board[winningIndex1]
+    check2 = board[winningIndex2]
+    check3 = board[winningIndex3]
+    
+    if check1 === check2 and check2 === check3
+      winner = true
+    end 
+  end
+  
+  if winner 
+    return true 
+  elsif !winner and board.include? ''
+    return false 
   end 
+  
 end 
+
+
+
+# def won(board)
+#   if board === []
+#     return "false"
+#   elsif board === ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+#     return "false"
+#   elsif board == WIN_COMBINATIONS
+#     return "true"
+#   end 
+#   end 
+# end 
   
